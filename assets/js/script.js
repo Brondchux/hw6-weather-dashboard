0084ce5e2b18434e1bef6e44b853fe9cb6dcfebf
Gospel Chukwu <brondchux@gmail.com>
2021-07-28 18:13:46 -0400

    Successfully displayed the dummy details of the selecetd city name via button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // PSEUDO CODE =============================================
 
 // DEPENDENCIES (DOM Elements) =============================
+let mainWeather = $("#main-weather");
+let searchButton = $("#search-button");
+let searchCity = $("#search-city");
+let alertMessages = $("#alert-messages");
+let cityButtons = $("#city-buttons");
 
 // DATA ====================================================
 let apiKey = "1517cd55cab8c75d16cd1fade8e7890d"; // Not secured
@@ -10,13 +15,94 @@ let localStorageName = "lookedUpCities";
 let cityNamesArr = [];
 
 // FUNCTIONS ===============================================
+// Primary function to run the app
+function initApp() {
+	// Ensure a city name was provided
+	if (!searchCity.val()) {
+		return alertMessages
+			.addClass("text-danger")
+			.text("A city name is required!");
+	}
+
+	// Clear out alert message if any
+	alertMessages.text("");
+
+	// Store city
+	storeCityName(searchCity.val());
+
+	// Fetch weather
+	fetchCityWeather(searchCity.val());
+
+	// Clear out search city
+	searchCity.val("");
+}
+
+// Update the main weather display section
+function displayMainWeather(weatherObj) {
+	if (!weatherObj) return;
+
+	// Create
+	let h2El = $("<h2>");
+	let pElTemp = $("<p>");
+	let pElWind = $("<p>");
+	let pElHumidity = $("<p>");
+	let pElUV = $("<p>");
+	let uvSpan = $("<span class='badge badge-success'>");
+
+	// Build
+	h2El.text(
+		`${weatherObj.city}, ${weatherObj.country.toUpperCase()} (${todaysDate()})`
+	);
+	pElTemp.text(`Temp: ${weatherObj.temp}`);
+	pElWind.text(`Wind: ${weatherObj.wind}`);
+	pElHumidity.text(`Humidity: ${weatherObj.humidity}`);
+	pElUV.text(`UV Index: `);
+	uvSpan.text(`TBA`);
+	pElUV.append(uvSpan);
+
+	// Place
+	mainWeather.append(h2El);
+	mainWeather.append(pElTemp);
+	mainWeather.append(pElWind);
+	mainWeather.append(pElHumidity);
+	mainWeather.append(pElUV);
+}
+
+// Display saved cites
+function listSavedCities() {
+	let cities = fetchStoredCities() ? fetchStoredCities() : [];
+
+	// Loop through the cities
+	cities.map((city) => {
+		// Create button
+		let buttonEl = $(
+			"<button class='btn btn-secondary form-control mt-2 text-capitalize'>"
+		);
+
+		// Build buttons
+		buttonEl.text(city);
+
+		// Place buttons
+		cityButtons.append(buttonEl);
+
+		// Add event listener
+		buttonEl.on("click", function () {
+			let buttonCityName = buttonEl.text();
+			fetchCityWeather(buttonCityName);
+		});
+	});
+}
+
 // Store the searched city name
 function storeCityName(newCityName) {
 	if (!newCityName) return;
-	cityNamesArr.unshift(newCityName);
+
+	// Combine the new array with the cities array from localstorage
+	let existingCitiesArr = fetchStoredCities() ? fetchStoredCities() : [];
+	let updatedCitiesArr = [...existingCitiesArr, newCityName];
 
 	// Save in local storage
-	localStorage.setItem(localStorageName, JSON.stringify(cityNamesArr));
+	localStorage.setItem(localStorageName, JSON.stringify(updatedCitiesArr));
 }
 
 // Fetch stored cities from local storage
@@ -26,15 +112,43 @@ function fetchStoredCities() {
 
 // Fetch base city weather details
 function fetchCityWeather(cityName) {
+	// Test data to save api calls
+	console.log("Weather details fetched for ", cityName);
+
+	let weatherObj = {
+		country: "Country",
+		city: "City",
+		temp: "Temp",
+		wind: "Wind",
+		humidity: "Humidity",
+	};
+	return weatherObj;
+
+	// Real data to use api calls
 	let requestUrl = `${baseUrl}q=${cityName}&appid=${apiKey}`;
 	fetch(requestUrl)
 		.then(function (response) {
 			return response.json();
 		})
 		.then(function (data) {
+			let weatherObj = {
+				country: data.sys.country,
+				city: data.name,
+				temp: data.main.temp,
+				wind: data.wind.speed,
+				humidity: data.main.humidity,
+			};
 			console.log(data);
 		});
 }
 
+// Display todays date
+function todaysDate() {
+	let today = Date.now();
+	return moment(today).format("MM/DD/YY");
+}
+
 // INITIALIZATION ==========================================
-fetchCityWeather(baseCity);
+displayMainWeather(fetchCityWeather(baseCity));
+searchButton.on("click", initApp);
+listSavedCities();
